4dd08f7260d66026c71328ce127100e12c51b66d
mrtung1999hpvn <tung75397@st.vimaru.edu.vn>
2020-11-23 02:09:34 +0700

    Updates

---
+++ precache-manifest.f50d334bbf4b1757ca92a95a2ee66742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989943069e86be328c4f842ed7dad6c7",
+    "url": "/btl-dinh-vi-xe/index.html"
+  },
+  {
+    "revision": "4cd38f10f1a79648e4e4",
+    "url": "/btl-dinh-vi-xe/static/css/main.4942f960.chunk.css"
+  },
+  {
+    "revision": "a2e9526121a188cd61fe",
+    "url": "/btl-dinh-vi-xe/static/js/2.909341a4.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/btl-dinh-vi-xe/static/js/2.909341a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd38f10f1a79648e4e4",
+    "url": "/btl-dinh-vi-xe/static/js/main.ed35b120.chunk.js"
+  },
+  {
+    "revision": "c77f32a387385b243383",
+    "url": "/btl-dinh-vi-xe/static/js/runtime-main.4085e6bc.js"
+  }
+]);
